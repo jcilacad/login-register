868847f11a552a114d49e114a81c2ba0a9b94b20
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-21 18:50:34 +0800

    Setup project

---
+++ src/main/java/com/ilacad/jc/mvcsecurityexercise/MvcSecurityExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ilacad.jc.mvcsecurityexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSecurityExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcSecurityExerciseApplication.class, args);
+	}
+
+}
