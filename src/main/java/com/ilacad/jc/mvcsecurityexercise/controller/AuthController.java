de819f30bf91cbf0524d54a1dc4e1d3523996cc8
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-25 16:29:08 +0800

    Add a success and error message in login module

--- src/main/java/com/ilacad/jc/mvcsecurityexercise/controller/AuthController.java
+++ src/main/java/com/ilacad/jc/mvcsecurityexercise/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.ilacad.jc.mvcsecurityexercise.dto.UserDto;
 import com.ilacad.jc.mvcsecurityexercise.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
