553ba7b172f827a28441afcf42a302acbc4592cb
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-25 12:36:45 +0800

    Configure the Authentication Controller Security

---
+++ src/main/java/com/ilacad/jc/mvcsecurityexercise/security/AuthControllerSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.ilacad.jc.mvcsecurityexercise.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthControllerSecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public AuthControllerSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/index/**").permitAll()
+                        .requestMatchers("/register/**").permitAll()
+                        .requestMatchers("/users/**").hasRole("ADMIN")
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/users")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                )
+                .exceptionHandling(handler -> handler
+                        .accessDeniedPage("/access-denied")
+                );
+
+        return http.build();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+
+}
